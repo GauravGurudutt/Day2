353aac47debaeab88725e83657bc7becb353ad48
GauravGurudutt <43572285+GauravGurudutt@users.noreply.github.com>
2018-09-25 18:50:06 +0530

    Create Name.c

---
+++ Name.c
@@ -0,0 +1,11 @@
+Write a program to get a number from the user and print it.
+
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter a number:");
+scanf("%d",&n);
+printf("The number you entered is:");
+printf("%d",n);
+}
